58731a25a710c8ecba612530cf1e16be9dcf3948
nosra <carson7331@gmail.com>
2025-05-03 22:54:40 -0400

    merge

--- frontend/wordle-app/app/routes/login.tsx
+++ frontend/wordle-app/app/routes/login.tsx
@@ -11,15 +11,29 @@ export function meta({}: Route.MetaArgs) {
   ];
 }
 
-const handleLogin = async () => {
+const handleLogin = async (e: any) => {
     console.log("sending user info!")
+    e.preventDefault();
+
+    // wrap the event in a FormData object
+    const formData = new FormData(e.target);
+    const username = formData.get("username")?.toString();
+    const password = formData.get("password")?.toString();
+
+    console.log(username?.toString());
+
     // construct a POST request to the user controller
     const res = await fetch("/api/user/", {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
         },
-        body: JSON.stringify({username: "hello!"}),
+        body: JSON.stringify(
+            {
+                username: username,
+                password: password,
+            }
+        ),
     })
     // TODO: do something with the response from the backend
 }
@@ -50,20 +64,21 @@ export default function Login() {
                             Create an Account
                         </label>
                     </div>
-                    <div className="login__login-box">
-                        <form>
-                        {/* username block */}
-                        <label className="block text-sm font-medium text-black pb-2">Username</label>
-                        <input type="text" id="username" className="bg-white border border-gray-300 text-sm rounded-lg block w-70 p-2.5 dark:placeholder-gray-400 " placeholder="skibidi@toilet.sigma" required />
+                    <form className="login__form" onSubmit={handleLogin}>
+                        <div className="login__login-box">
+                            {/* username block */}
+                            <label className="block text-sm font-medium text-black pb-2">Username</label>
+                            <input type="text" name="username" className="bg-white border border-gray-300 text-sm rounded-lg block w-70 p-2.5 dark:placeholder-gray-400 " placeholder="skibidi@toilet.sigma" required />
 
-                        {/* password block */}
-                        <label className="block text-sm font-medium text-black pb-2 pt-10">Password</label>
-                        <input type="password" id="password" className="bg-white border border-gray-300 text-sm rounded-lg block w-70 p-2.5 dark:placeholder-gray-400" placeholder="Enter Password" required />
-                        </form>
-                    </div>
-                    <div className="flex rounded-2xl transition duration-300 ease-in-out hover:bg-green-700 text-white pt-3 pb-3 mt-5 w-70 justify-center bg-[#3cb53f]">
-                        <button className="login__continue w-full" onClick={handleLogin}>Continue</button>
-                    </div>
+                            {/* password block */}
+                            <label className="block text-sm font-medium text-black pb-2 pt-10">Password</label>
+                            <input type="password" name="password" className="bg-white border border-gray-300 text-sm rounded-lg block w-70 p-2.5 dark:placeholder-gray-400" placeholder="Enter Password" required />
+                            
+                        </div>
+                        <div className="flex rounded-2xl transition duration-300 ease-in-out hover:bg-green-700 text-white pt-3 pb-3 mt-5 w-70 justify-center bg-[#3cb53f]">
+                            <button className="login__continue w-full" type="submit">Continue</button>
+                        </div>
+                    </form>
                     <div className="flex w-70 justify-start">
                         <a className="text-slate-400 mt-10 underline">Forgot Password?</a>
                     </div>
