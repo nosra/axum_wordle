1a1bc2bb253aefb1643e7c26003b63126c6f073b
nosra <77589065+nosra@users.noreply.github.com>
2025-05-14 18:05:51 -0400

    ...tuning response design

--- frontend/wordle-app/app/usergamelist/usergamelist.tsx
+++ frontend/wordle-app/app/usergamelist/usergamelist.tsx
@@ -61,7 +61,7 @@ export function UserGameList() {
     return (
         <>
         <div className="board-container bg-[#eeeeee] h-[calc(100vh-60px)] overflow-auto overflow-x-hidden">
-            <div className="board-list flex-1 grid lg:grid-cols-3 grid-cols-2 gap-x-5 gap-y-5 p-5 min-w-max">
+            <div className="board-list flex-1 grid max-[600px]:grid-cols-3 grid-cols-2 gap-x-5 gap-y-5 p-5 min-w-max">
                 {/* TODO: query the database for a list of all games */}
                 {boardList?.map((board, boardNum) => (
                     <div key={boardNum} 
